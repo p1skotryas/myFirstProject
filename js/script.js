be92b2aa78c814e1c1c3dd3c6a03bf4fd275afe4
Andrey <example@gmail.com>
2020-11-23 18:58:28 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// console.log(name);
+// var name = 'Andrey';
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
+
+// let number = 4.6; 
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// alert('Hello'); 
+
+// const result = confirm("Are u here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Ваша фамилия?', '');   
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Andrey";
+
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++ стоит перед - префиксная форма, -- стоит после - это постфиксная 
+
+// ++incr;
+// --decr;    
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+
+// && - и ; || - или ! - оператор отрицания
+
+const isChecked = true,         
+      isClose = false;
+
+console.log(isChecked || isClose);      
